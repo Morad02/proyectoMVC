38bfa023b6a67fcc8301e78df51c81ed678e4b6a
Morad02 <kharraz02@correo.ugr.es>
2023-05-30 16:56:30 +0200

    Nuevo usuario se puede añadir a la base de datos

--- proyecto/controladores/GestionUsuario.php
+++ proyecto/controladores/GestionUsuario.php
@@ -1,10 +1,12 @@
 <?php
 
+
     class GestionUsuario extends Controlador
     {
         public function __construct() 
         {
             $this->usuarioModelo = $this->cargarModelo('Usuario');
+            $this->request = new Request();
         }
 
         public function index()
@@ -12,6 +14,31 @@
             $datos = [];
             $datos['usuarios'] = $this->usuarioModelo->obtenerUsuarios();
             $this->cargarVista('gestionUsuario/index', $datos);
+        }
+
+        public function agregar()
+        {
+            echo "Estoy en agregar";
+            if($_SERVER['REQUEST_METHOD'] === 'POST')
+            {
+                echo "Estoy en el post";
+                $nombre = $this->request->get_Dato('nuevoNombre');
+                $apellidos= $this->request->get_Dato('nuevoApellidos');
+                $email = $this->request->get_Email('nuevoEmail');
+                $direccion = $this->request->get_Dato('nuevoDireccion');
+                $telefono = $this->request->get_Telefono('nuevoTelefono');
+                $rol = $this->request->get_Dato('nuevoRol');
+                $estado = $this->request->get_Dato('nuevoEstado');
+                $img = $this->request->get_Imagen($_FILES,'nuevoFoto');
+
+                $password = $this->request->get_Password('nuevoClave1');
+                
+                //echo $_POST['nuevoTelefono'];
+                echo $telefono;
+                $this->usuarioModelo->nuevoUsuario($email,$nombre,$apellidos,$password,$telefono,$direccion,$img,$estado,$rol);
+                
+                $this->
+            }
         }
 
         
