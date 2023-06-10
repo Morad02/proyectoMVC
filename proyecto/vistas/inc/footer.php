c9ea1dde4293c1c85871f3d0bc9ff627d24eaf1c
Carlos Servillera <carlosservillera@gmail.com>
2023-06-10 19:16:08 +0200

    commit

--- proyecto/vistas/inc/footer.php
+++ proyecto/vistas/inc/footer.php
@@ -1,5 +1,6 @@
-                <div class="card-footer text-muted">
-                        2 days ago
+                <div class="card-footer text-muted d-flex justify-content-between align-items-center">
+                  <p class="mr-auto">Morad Kharraz y Carlos Servillera</p>
+                  <a href="#" class="text-center">Documentación</a>
                 </div>
                 <!--scripts-->
                 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
